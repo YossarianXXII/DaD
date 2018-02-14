58b190d7e62a76daa49db8a08744957390259b74
michal.szeman <miso.sze@gmail.com>
2018-02-14 14:17:29 +0100

    Add GUI utility methods

---
+++ src/gui/GUIUtil.java
@@ -0,0 +1,38 @@
+package gui;
+
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.shape.Rectangle;
+
+public class GUIUtil {
+
+
+    /**
+     * Clips the children of the specified {@link Region} to its current size.
+     * This requires attaching a change listener to the region’s layout bounds,
+     * as JavaFX does not currently provide any built-in way to clip children.
+     *
+     * @param region the {@link Region} whose children to clip
+     * @throws NullPointerException if {@code region} is {@code null}
+     */
+    public static void clipChildren(Region region) {
+        final Rectangle outputClip = new Rectangle();
+        region.setClip(outputClip);
+        region.layoutBoundsProperty().addListener((ov, oldValue, newValue) -> {
+            outputClip.setWidth(newValue.getWidth());
+            outputClip.setHeight(newValue.getHeight());
+        });
+    }
+
+    public static void anchorAll(Node node){
+        AnchorPane.setTopAnchor(node, 0.0);
+        AnchorPane.setBottomAnchor(node, 0.0);
+        AnchorPane.setLeftAnchor(node, 0.0);
+        AnchorPane.setRightAnchor(node, 0.0);
+    }
+
+
+
+}
